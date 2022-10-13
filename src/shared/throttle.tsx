b71f6080ec26593be52e486c6d9add5560e00ae4
FrankLai <laihongjiang@foxmail.com>
2022-10-14 00:13:00 +0800

    TS 体操 , 完善 throttle

--- src/shared/throttle.tsx
+++ src/shared/throttle.tsx
@@ -1,10 +1,13 @@
-export const throttle = (fn: Function, time: number) => {
+export const throttle = <T extends ((...args: unknown[]) => any)>(fn: T, time: number) => {
     let timer: number | undefined = undefined
-    return (...args: any[]) => {
-        if(timer) { return }
-        fn(...args)
-        timer = setTimeout(() => {
-            timer = undefined
-        },time)
+    let result: ReturnType<T>
+    return (...args: Parameters<T>) => {
+        if (!timer) {
+            result = fn(...args)
+            timer = setTimeout(() => {
+                timer = undefined
+            }, time)
+        }
+        return result
     }
 }
