262630a3583c013f767314f56e0fc7f924bb9358
FrankLai <laihongjiang@foxmail.com>
2022-11-01 09:41:13 +0800

    完成 TagEdit 页面的样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -66,6 +66,7 @@ export const ItemCreate = defineComponent({
           <div class={s.wrapper}>
             <Tabs v-model:selected={refKind.value} class={s.tabs}>
               <Tab name="支出" class={s.tags_wrapper}>
+                h1
                 <div class={s.tag}>
                   <div class={s.sign}>
                     <Icon name="add" class={s.createTag} />
