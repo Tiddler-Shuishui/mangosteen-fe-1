fadeea6cb3d08707941e4acf2250cf7696e878fb
FrankLai <laihongjiang@foxmail.com>
2022-11-14 17:04:42 +0800

    重构 TagForm ，封装 Form 组件

--- src/shared/EmojiSelect.tsx
+++ src/shared/EmojiSelect.tsx
@@ -5,6 +5,9 @@ export const EmojiSelect = defineComponent({
   props: {
     modelValue: {
       type: String
+    },
+    onUpdateModelValue: {
+      type: Function as PropType<(emoji:string) => void>
     }
   },
   setup: (props, context) => {
@@ -25,7 +28,11 @@ export const EmojiSelect = defineComponent({
       refSelected.value = index
     } 
     const onClickEmoji = (emoji: string) => {
-      context.emit('update:modelValue',emoji)
+      if (props.onUpdateModelValue) {
+        props.onUpdateModelValue(emoji)
+      } else {
+        context.emit('update:modelValue', emoji)
+      }
     }
     const emojis = computed(() => {
       const selectedItem = table[refSelected.value][1]
