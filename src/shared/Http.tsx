0b2df1baf537b67bf574e67a7e18b84f2f6984c5
FrankLai <laihongjiang@foxmail.com>
2023-03-17 20:19:48 +0800

    创建 mock 模块

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,6 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import { Mock, mockSession } from "../mock/mock";
 
-type JSONValue = string | number | null | boolean | JSONValue[] | {[key: string]: JSONValue};
+type GetConfig = Omit<AxiosRequestConfig,'params' | 'url' | 'method'>
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>
+type DeleteConfig = Omit<AxiosRequestConfig, 'params'>
 
 export class Http {
   instance: AxiosInstance
@@ -8,22 +12,42 @@ export class Http {
     this.instance = axios.create({baseURL})
   }
   // read
-  get<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig,'params' | 'url' | 'method'>){
+  get<R = unknown>(url: string, query?: Record<string, string>, config?: GetConfig){
     return this.instance.request<R>({ ...config, url, params: query, method: 'get' })
   }
   // create
-  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>){
+  post<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PostConfig){
     return this.instance.request<R>({ ...config, url, data, method:'post'})
   }
   // update
-  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: Omit<AxiosRequestConfig, 'url' | 'data'>){
+  patch<R = unknown>(url: string, data?: Record<string, JSONValue>, config?: PatchConfig){
     return this.instance.request<R>({ ...config, url, data, method: 'patch' })
   }
   // destory
-  delete<R = unknown>(url: string, query?: Record<string, string>, config?: Omit<AxiosRequestConfig, 'params'>){
+  delete<R = unknown>(url: string, query?: Record<string, string>, config?: DeleteConfig){
     return this.instance.request<R>({ ...config, url, params: query, method: 'delete' })
   }
-} 
+}
+
+const mockList:Record<string, Mock> = {
+  // 'tagIndex': mockTagIndex,
+  // 'itemCreate': mockItemCreate,
+  // 'itemIndex': mockItemIndex,
+  // 'tagCreate': mockTagCreate,
+  'session': mockSession
+
+}
+
+const mock = (response: AxiosResponse) => {
+  if(['localhost','127.0.0.1','192.168.3.57'].indexOf(location.hostname) === -1){return false}
+
+  const mockName = response.config?.params?._mock
+  if(Object.keys(mockList).includes(mockName)){
+    [response.status, response.data] = mockList[mockName](response.config)
+    return true
+  }
+  return false
+}
 
 export const http = new Http('/api/v1')
 
@@ -33,6 +57,17 @@ http.instance.interceptors.request.use(config => {
     config.headers!.Authorization = `Bearer ${jwt}`
   }
   return config
+})
+
+http.instance.interceptors.response.use(response => {
+  mock(response)
+  return response
+},(error)=>{
+  if(mock(error.response)){
+    return error.response
+  }else{
+    throw error
+  }
 })
 
 http.instance.interceptors.response.use(response => {
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -39,7 +39,10 @@ export const SignInPage = defineComponent({
         {key: 'code', type: 'required', message: '必填'}
       ]))
       if(!hasError(errors)){
-        const response = await http.post<{ jwt: string }>('/session', formData)
+        const response = await http.post<{ jwt: string }>('/session', formData, {
+          params: { _mock: 'session' }
+        }).catch(onError)
+        console.log(response)
         localStorage.setItem('jwt', response.data.jwt)
         const returnTo = route.query.return_to?.toString()
         refreshMe()
