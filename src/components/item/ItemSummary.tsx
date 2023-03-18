554819428f53643cb1b4ffdd4b9975f708d2de57
FrankLai <laihongjiang@foxmail.com>
2023-03-18 21:26:09 +0800

    要求后端再 items 中嵌套 tags

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -46,7 +46,6 @@ export const ItemSummary = defineComponent({
         page: page.value + 1,
         _mock: 'itemIndexBalance'
       })
-      console.log(response)
       Object.assign(itemsBalance, response.data)
     })
     return () => (
@@ -71,11 +70,11 @@ export const ItemSummary = defineComponent({
               {items.value.map((item) => (
                 <li>
                   <div class={s.sign}>
-                    <span>{item.tags_id[0]}</span>
+                    <span>{item.tags![0].sign}</span>
                   </div>
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
-                      <span class={s.tag}>{item.tags_id[0]}</span>
+                      <span class={s.tag}>{item.tags![0].name}</span>
                       <span class={s.amount}>
                         ￥<Money value={item.amount} />
                       </span>
