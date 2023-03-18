c3ed8c8c2ce9457d19026d226078d544b5cec02f
FrankLai <laihongjiang@foxmail.com>
2023-03-18 21:04:24 +0800

    封装 DateTime

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, onMounted, PropType, ref } from 'vue'
 import { Button } from '../../shared/Button'
+import { DateTime } from '../../shared/Datetime'
 import { FloatButton } from '../../shared/FloatButton'
 import { http } from '../../shared/Http'
 import { Money } from '../../shared/Money'
@@ -61,9 +62,13 @@ export const ItemSummary = defineComponent({
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
                       <span class={s.tag}>{item.tags_id[0]}</span>
-                      <span class={s.amount}>￥<Money value={item.amount} /></span>
+                      <span class={s.amount}>
+                        ￥<Money value={item.amount} />
+                      </span>
                     </div>
-                    <div class={s.time}>{item.happen_at}</div>
+                    <div class={s.time}>
+                      <DateTime value={item.happen_at} />
+                    </div>
                   </div>
                 </li>
               ))}
