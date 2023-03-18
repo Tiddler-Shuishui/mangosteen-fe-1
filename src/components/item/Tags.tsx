1d038b803543c13b847c857418c305ad216a017a
FrankLai <laihongjiang@foxmail.com>
2023-03-18 17:20:03 +0800

    长按 tag 跳转到编辑页面

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, PropType, ref } from 'vue'
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRouter } from 'vue-router'
 import { useTags } from '../../hooks/useTags'
 import { Button } from '../../shared/Button'
 import { http } from '../../shared/Http'
@@ -29,13 +29,14 @@ export const Tags = defineComponent({
     }
     const timer = ref<number>()
     const currentTag = ref<HTMLDivElement>()
-    const onLongPress = () => {
-      console.log('长按')
+    const router = useRouter()
+    const onLongPress = (tagId: Tag['id']) => {
+      router.push(`/tags/${tagId}/edit?kind=${props.kind}&return_to=${router.currentRoute.value.fullPath}`)
     }
-    const onTouchStart = (e: TouchEvent) => {
+    const onTouchStart = (e: TouchEvent, tag: Tag) => {
       currentTag.value = e.currentTarget as HTMLDivElement
       timer.value = setTimeout(()=>{
-        onLongPress()
+        onLongPress(tag.id)
       },500)
     }
     const onTouchEnd = (e: TouchEvent) => {
@@ -60,7 +61,7 @@ export const Tags = defineComponent({
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : '']}
               onClick={() => onSelect(tag)}
-              onTouchstart={onTouchStart}
+              onTouchstart={(e)=>onTouchStart(e, tag)}
               onTouchend={onTouchEnd}
             >
               <div class={s.sign}>{tag.sign}</div>
