d879deeee87aa8da3062cfff4b2d051491ace19e
FrankLai <laihongjiang@foxmail.com>
2023-03-18 12:19:36 +0800

    配置 prettier

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -8,85 +8,161 @@ export const InputPad = defineComponent({
     happenAt: String,
     amount: Number,
     onSubmit: {
-      type: Function as PropType<() => void>
-    }
+      type: Function as PropType<() => void>,
+    },
   },
   emits: ['update:happenAt', 'update:amount'],
   setup: (props, context) => {
-    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0')
+    const refAmount = ref(props.amount ? (props.amount / 100).toString() : '0');
     const appendText = (n: number | string) => {
-      const nString = n.toString()
-      const dotIndex = refAmount.value.indexOf('.')
+      const nString = n.toString();
+      const dotIndex = refAmount.value.indexOf('.');
       if (refAmount.value.length >= 13) {
-        return
+        return;
       }
       if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
-        return
+        return;
       }
       if (nString === '.') {
-        if (dotIndex >= 0) { // 已经有小数点了
-          return
+        if (dotIndex >= 0) {
+          // 已经有小数点了
+          return;
         }
       } else if (nString === '0') {
-        if (dotIndex === -1) { // 没有小数点
-          if (refAmount.value === '0') { // 没小数点，但是有0
-            return
+        if (dotIndex === -1) {
+          // 没有小数点
+          if (refAmount.value === '0') {
+            // 没小数点，但是有0
+            return;
           }
         }
       } else {
         if (refAmount.value === '0') {
-          refAmount.value = ''
+          refAmount.value = '';
         }
       }
-      refAmount.value += n.toString()
-    }
+      refAmount.value += n.toString();
+    };
     const buttons = [
       {
-        text: '1', onClick: () => { appendText(1) }
+        text: '1',
+        onClick: () => {
+          appendText(1);
+        },
       },
-      { text: '2', onClick: () => { appendText(2) } },
-      { text: '3', onClick: () => { appendText(3) } },
-      { text: '4', onClick: () => { appendText(4) } },
-      { text: '5', onClick: () => { appendText(5) } },
-      { text: '6', onClick: () => { appendText(6) } },
-      { text: '7', onClick: () => { appendText(7) } },
-      { text: '8', onClick: () => { appendText(8) } },
-      { text: '9', onClick: () => { appendText(9) } },
-      { text: '.', onClick: () => { appendText('.') } },
-      { text: '0', onClick: () => { appendText(0) } },
-      { text: '清空', onClick: () => { refAmount.value = '0' } },
       {
-        text: '提交', onClick: () => {
-          context.emit('update:amount', parseFloat(refAmount.value) * 100)
-          props.onSubmit?.()
-        }
+        text: '2',
+        onClick: () => {
+          appendText(2);
+        },
       },
-    ]
-    const refDatePickerVisible = ref(false)
-    const showDatePicker = () => refDatePickerVisible.value = true
-    const hideDatePicker = () => refDatePickerVisible.value = false
-    const setDate = (date: Date) => { context.emit('update:happenAt', date.toISOString()); hideDatePicker() }
+      {
+        text: '3',
+        onClick: () => {
+          appendText(3);
+        },
+      },
+      {
+        text: '4',
+        onClick: () => {
+          appendText(4);
+        },
+      },
+      {
+        text: '5',
+        onClick: () => {
+          appendText(5);
+        },
+      },
+      {
+        text: '6',
+        onClick: () => {
+          appendText(6);
+        },
+      },
+      {
+        text: '7',
+        onClick: () => {
+          appendText(7);
+        },
+      },
+      {
+        text: '8',
+        onClick: () => {
+          appendText(8);
+        },
+      },
+      {
+        text: '9',
+        onClick: () => {
+          appendText(9);
+        },
+      },
+      {
+        text: '.',
+        onClick: () => {
+          appendText('.');
+        },
+      },
+      {
+        text: '0',
+        onClick: () => {
+          appendText(0);
+        },
+      },
+      {
+        text: '清空',
+        onClick: () => {
+          refAmount.value = '0';
+        },
+      },
+      {
+        text: '提交',
+        onClick: () => {
+          context.emit('update:amount', parseFloat(refAmount.value) * 100);
+          props.onSubmit?.();
+        },
+      },
+    ];
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => (refDatePickerVisible.value = true);
+    const hideDatePicker = () => (refDatePickerVisible.value = false);
+    const setDate = (date: Date) => {
+      context.emit('update:happenAt', date.toISOString());
+      hideDatePicker();
+    };
 
-    return () => <>
-      <div class={s.dateAndAmount}>
-        <span class={s.date}>
-          <Icon name="date" class={s.icon} />
-          <span>
-            <span onClick={showDatePicker}>{new Time(props.happenAt).format()}</span>
-            <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
-              <DatetimePicker value={props.happenAt} type="date" title="选择年月日"
-                onConfirm={setDate} onCancel={hideDatePicker}
-              />
-            </Popup>
+    return () => (
+      <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon name="date" class={s.icon} />
+            <span>
+              <span onClick={showDatePicker}>
+                {new Time(props.happenAt).format()}
+              </span>
+              <Popup
+                position="bottom"
+                v-model:show={refDatePickerVisible.value}
+              >
+                <DatetimePicker
+                  value={props.happenAt}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={setDate}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
+            </span>
           </span>
-        </span>
-        <span class={s.amount}>{refAmount.value}</span>
-      </div>
-      <div class={s.buttons}>
-        {buttons.map(button =>
-          <button onClick={button.onClick}>{button.text}</button>
-        )}
-      </div>
-    </>
-  }
-})
+          <span class={s.amount}>{refAmount.value}</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((button) => (
+            <button onClick={button.onClick}>{button.text}</button>
+          ))}
+        </div>
+      </>
+    );
+  },
+});
