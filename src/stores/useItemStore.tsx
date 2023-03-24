afcd6c6559772aef93fc5acaa660c690606b91cb
FrankLai <laihongjiang@foxmail.com>
2023-03-24 17:37:57 +0800

    修复错误地请求了下一页 items 的 bug

--- src/stores/useItemStore.tsx
+++ src/stores/useItemStore.tsx
@@ -1,5 +1,5 @@
-import { defineStore } from "pinia"
-import { http } from "../shared/Http"
+import { defineStore } from 'pinia'
+import { http } from '../shared/Http'
 
 type State = {
   items: Item[]
@@ -9,11 +9,13 @@ type State = {
 
 type Actions = {
   reset: () => void
-  fetchItems: (stateDate?: string, endDate?: string) => void
+  _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void
+  fetchItems: (startDate?: string, endDate?: string) => void
+  fetchNextPage: (startDate?: string, endDate?: string) => void
 }
 
-export const useItemStore = (id: string | string[]) => 
-  defineStore<string, State, {}, Actions>(typeof id === 'string' ? id : id.join('-'),{
+export const useItemStore = (id: string | string[]) =>
+  defineStore<string, State, {}, Actions>(typeof id === 'string' ? id : id.join('-'), {
     state: () => ({
       items: [],
       hasMore: false,
@@ -25,20 +27,36 @@ export const useItemStore = (id: string | string[]) =>
         this.hasMore = false
         this.page = 0
       },
-      async fetchItems(stateDate, endDate){
-        if(!stateDate || !endDate){ return }
-        const response = await http.get<Resources<Item>>('/items', {
-          happen_after: stateDate,
-          happen_before: endDate,
-          page: this.page + 1
-        },{
-          _mock: 'itemIndex',
-          _autoLoading: true
-        })
+      async _fetch(firstpage, startDate, endDate) {
+        if (!startDate || !endDate) {
+          return
+        }
+        const response = await http.get<Resources<Item>>(
+          '/items',
+          {
+            happen_after: startDate,
+            happen_before: endDate,
+            page: firstpage ? 1 : this.page + 1
+          },
+          {
+            _mock: 'itemIndex',
+            _autoLoading: true
+          }
+        )
         const { resources, pager } = response.data
-        this.items?.push(...resources)
+        if (firstpage) {
+          this.items = resources
+        } else {
+          this.items.push(...resources)
+        }
         this.hasMore = (pager.page - 1) * pager.per_page + resources.length < pager.count
         this.page += 1
+      },
+      async fetchNextPage(startDate, endDate) {
+        this._fetch(false, startDate, endDate)
+      },
+      async fetchItems(startDate, endDate) {
+        this._fetch(true, startDate, endDate)
       }
     }
   })()
